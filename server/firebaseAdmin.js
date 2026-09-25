2c8b8bee1af700b2a55ab368d1ecefa684b1ce00
lakshitthakur <lakshitthakur224@gmail.com>
2026-09-25 17:50:38 +1000

    Configure Firebase Admin securely with environment variables

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -1,10 +1,27 @@
 const { initializeApp, cert } = require('firebase-admin/app');
+
 const { getFirestore } = require('firebase-admin/firestore');
 
-const serviceAccount = require('./serviceAccountKey.json');
+const projectId = process.env.FIREBASE_PROJECT_ID;
+
+const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
+
+const privateKey = process.env.FIREBASE_PRIVATE_KEY
+  ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+  : undefined;
+
+if (!projectId || !clientEmail || !privateKey) {
+  throw new Error(
+    'Firebase Admin environment variables are missing.'
+  );
+}
 
 initializeApp({
-  credential: cert(serviceAccount)
+  credential: cert({
+    projectId,
+    clientEmail,
+    privateKey
+  })
 });
 
 const db = getFirestore();
